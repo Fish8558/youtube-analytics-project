68bd9bc30970240af63f6a0f3e0566336beb9a30
Stas <stas_sever@mail.ru>
2024-01-06 21:15:40 +0500

    YT

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,11 @@
+import json
+import os
+
+from googleapiclient.discovery import build
+
+channel_id = os.getenv('YT_API_KEY')
+
+youtube = build('youtube', 'v3', developerKey=channel_id)
 
 
 class Channel:
@@ -5,8 +13,8 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
-    def print_info(self) -> None:
+    def print_info(self, dict_to_print: dict) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
